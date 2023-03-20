d5eb788d028fe7db55a87c7047f15c8a307540c2
lucqaz01 <Lucassobczak3@gmail.com>
2023-03-19 20:23:09 -0400

    Initialized express. Confirmed server response

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cors = require('cors')
+const videos = require('./routes/videos')
 
 app.use(express.json());
 app.use(cors())
 app.use(express.static("images"))
+app.use('/videos', videos)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
